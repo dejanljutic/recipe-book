cd09507644d570a1ee28034aaac771d3b3ee856e
Dejan Ljutic <dejan.ljutic@devcenter.rs>
2019-11-04 14:21:49 +0100

    initial commit

---
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pancakes', 'Flour, milk, eggs', 'https://www.theworktop.com/wp-content/uploads/2019/02/Thin-Pancakes-Recipe-1x1.jpg'),
+    new Recipe('Burek', 'Kore, meso, luk', 'https://i2.wp.com/kuhinjazaposlenezene.com/wp-content/uploads/2019/07/Burek-blog-2.jpg?fit=1200%2C800&ssl=1')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
